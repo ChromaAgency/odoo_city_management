6fb0986f64206393cd29004761fd80b519c930d6
Federico Gobea <127751411+making-fgobea@users.noreply.github.com>
2023-10-02 08:23:37 -0500

    🐛 get report by name

--- city_management/controller/report.py
+++ city_management/controller/report.py
@@ -2,6 +2,8 @@ from odoo.http import request, Response, Controller, route
 import logging
 import json
 
+from ..models.city_report import REPORT_STATES
+
 _logger = logging.getLogger(__name__)
 
 BASE_URL = '/city_management'
@@ -13,12 +15,27 @@ class CategoriesController(Controller):
     def base_url(self):
         return request.env['ir.config_parameter'].sudo().get_param('web.base.url')
 
-    @route(f"{CHATBOT_OPTIONS_URL}/report/name/<name>", type='http', auth='none', methods=['GET'], csrf=False, cors="*")
+    @route(f"{BASE_URL}/report/name/<name>", type='http', auth='none', methods=['GET'], csrf=False, cors="*")
     def geocode_get_address(self, name):
-        report = request.env['city.report'].sudo().search_read([('name', '=', name)], ["name", "category_id", "subcategory_id"], limit=1)
+        CityReport = request.env['city.report'].sudo()
+        report = CityReport.search_read([('name', '=', name)], ["name", "category_id", "subcategory_id", "state"], limit=1)
         if report:
+            log = request.env['city.report.state.log'].sudo().search([('report_id', '=', report[0] ['id'])], limit=1)
+            res = report[0]
+            res.update({
+                
+                "state": dict(CityReport._fields['state']._description_selection(request.env)).get(res["state"])
+            })
+            if log:
+                log_create_date = log.create_date
+                _logger.info(log_create_date)
+                res.update({
+                    "last_change_date": log_create_date.strftime("%d/%m/%Y"),
+                    "last_change_time": log_create_date.strftime("%H:%M"),
+                    # "last_change_user": log.user_id.name,	
+                })
             response_body = {
-                "result": report[0],
+                "result": res,
                 
             }
         else:
