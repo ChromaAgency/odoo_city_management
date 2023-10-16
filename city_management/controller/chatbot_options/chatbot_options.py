369f8e3aff86b66da301d9249b1a739aa9ea9e7c
Federico Gobea <127751411+making-fgobea@users.noreply.github.com>
2023-10-15 21:56:18 -0500

    ✨ add chatbot max and min options

--- city_management/controller/chatbot_options/chatbot_options.py
+++ city_management/controller/chatbot_options/chatbot_options.py
@@ -14,8 +14,13 @@ class ChatBotOptions(Controller):
 
     @route(f"{CHATBOT_OPTIONS_URL}/categories", type='http', auth='none', methods=['GET'], csrf=False, cors="*")
     def categories_route(self):
+        categories = request.env['city.report.category'].sudo().search_read([('parent_id', '=', False)], ["name", "code"])
         response_body = {
-            "result": "\n".join([f"{category['code']} - {category['name']}\n" for category in request.env['city.report.category'].sudo().search_read([('parent_id', '=', False)], ["name", "code"])]),
+            "result": "\n".join(f"{category['code']} - {category['name']}\n" for category in categories),
+            "filters":{
+                "maxOption":max(category['code'] for category in categories),
+                "minOption":min(category['code'] for category in categories),
+            }
             
         }
         resp = Response(json.dumps(response_body), status=200)
@@ -24,9 +29,13 @@ class ChatBotOptions(Controller):
     
     @route(f"{CHATBOT_OPTIONS_URL}/categories/<int:category_id>/subcategories", type='http', auth='none', methods=['GET'], csrf=False, cors="*")
     def subcategories_route(self, category_id):
+        categories = request.env['city.report.category'].sudo().search_read([('parent_id', '=', category_id)], ["name", "code"])
         response_body = {
-            "result": "\n".join([f"{category['code']} - {category['name']}\n" for category in request.env['city.report.category'].sudo().search_read([('parent_id', '=', category_id)], ["name", "code"])]),
-            
+            "result": "\n".join(f"{category['code']} - {category['name']}\n" for category in categories),
+            "filters":{
+                "maxOption":max(category['code'] for category in categories),
+                "minOption":min(category['code'] for category in categories),
+            }
         }
         resp = Response(json.dumps(response_body), status=200)
         resp.headers['Content-Type'] = 'application/json'
--- whatsapp/models/city_report.py
+++ whatsapp/models/city_report.py
@@ -6,17 +6,40 @@ import logging
 _logger = logging.getLogger(__name__)
 
 
-
 class CityReport(Model):
     _inherit = "city.report"
 
-    def send_whatsapp_message(self, message):
+    def send_whatsapp_message(self, message, params):
         if not self.mobile:
             return
         wa_api = WhatsAppApi(self.env["whatsapp.account"].search([], limit=1))
-        wa_api.send_whatsapp(self.mobile, "text", {"body": message})
-    
+        try:
+            wa_api.send_whatsapp(self.mobile, "text", {"body": message})
+        except Exception as e:
+            wa_api.send_whatsapp(self.mobile, "template", params)
+
     def mark_as_in_progress(self):
         _ = super().mark_as_in_progress()
-        self.send_whatsapp_message(f"Tu solicitud {self.name} ha sido aceptada. En los próximos días será solucionado. Gracias por tu colaboración.")
+        state = dict(self._fields['state']._description_selection(
+            self.env)).get(self.state)
+        name = self.name
+        self.send_whatsapp_message(f"Tu solicitud {name} ha sido {state}. En los próximos días será solucionado. Gracias por tu colaboración.", 
+                                   {"namespace": "odooManyChatHypear",
+                                    "name": "update_estado_reporte",
+                                    "language": {
+                                        "code": "es",
+                                        "policy": "deterministic"
+                                    }, "components": [{
+                                        "type": "body",
+                                        "parameters": [
+                                            {
+                                                "type": "text",
+                                                "text": name
+                                            },
+                                            {
+                                                "type": "text",
+                                                "text": state
+                                            },
+
+                                        ]}]})
         return _
